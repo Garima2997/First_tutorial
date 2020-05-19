c088c6350a952398f3246e72106307bf7d046a36
Garima2997 <63338418+Garima2997@users.noreply.github.com>
2020-05-19 20:59:18 +0530

    Update sending_mail.py

--- sending_mail.py
+++ sending_mail.py
@@ -7,7 +7,7 @@ from pathlib import Path
 html = Template(Path('test.html').read_text())
 email = EmailMessage()
 email['from'] = 'Garima Rahangdale'
-email['to'] = 'garimarahangdale6@gmail.com'
+email['to'] = 'xyz@gmail.com'
 email['subject'] = 'just checking my program!!'
 
 email.set_content(html.substitute(name = 'Garima'), 'html')                                            
@@ -15,6 +15,6 @@ email.set_content(html.substitute(name = 'Garima'), 'html')
 with smtplib.SMTP(host = 'smtp.gmail.com', port = 587) as smtp:
 	smtp.ehlo()
 	smtp.starttls()
-	smtp.login('garimarahangdale6@gmail.com', 'gar2997raha')
+	smtp.login('xyz@gmail.com', 'password')
 	smtp.send_message(email)
 	print('Email sent!!')
